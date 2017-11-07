03e0f2b15410a1eee12daa3ec5171812d5bbdda1
Alex Renda <alex@renda.org>
2017-11-07 13:50:12 -0500

    first pass at language

--- language.v
+++ language.v
@@ -5,10 +5,23 @@ Definition ptr := nat.
 Definition ptr_eq_dec := eq_nat_dec.
 
 (** Types of values we can have in the heap *)
-Inductive val : Type := 
-  | Nat_t : nat -> val
-  | Pointer_t : ptr -> val.
+Inductive val : Type :=
+  | Nat : nat -> val
+  | Pointer : ptr -> val
+  | Struct : list val -> val.
 
 (** We will model heaps as lists of pointers and values. *)
 Definition heap := list (ptr * val).
 
+Inductive valexp :=
+| PtrRead : ptr -> valexp
+| Deref : ptr -> nat -> valexp.
+
+Inductive com :=
+| Skip : com
+| New : ptr -> valexp -> com
+| Assign : ptr -> valexp -> com
+| Drop : ptr -> com
+| Out : valexp -> com.
+Open Scope type.
+Definition state := (list com * list ptr * heap).
