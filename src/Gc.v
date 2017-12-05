016c75b1292ae5fd2dacf85ce369c3b93e0421ca
Matthew Li <ml927@cornell.edu>
2017-12-04 20:47:05 -0500

    Started moving some proofs to util

--- src/Gc.v
+++ src/Gc.v
@@ -1,4 +1,4 @@
-Require Import Gc.Language.
+Require Import Gc.Language Gc.Util.
 Require Import List ListSet Equality CpdtTactics.
 Require Import Coq.Program.Wf Coq.Logic.ProofIrrelevance FunInd Recdef.
 
@@ -676,50 +676,6 @@ Fixpoint sweep (h: heap_t) (ptrs: set ptr) : heap_t :=
 Definition gc (r: roots_t) (h: heap_t) : heap_t :=
   sweep h (mark r h)
 .
-
-Lemma in_split_l_tl :
-  forall {A B: Type} (l: list (A * B)) (p o: A) (v: B),
-    In p (fst (split l)) ->
-    In p (fst (split ((o, v) :: l))).
-Proof.
-  induction l. crush.
-  crush.
-  specialize (IHl p o v).
-  destruct (split l). crush.
-Qed.
-
-Lemma in_split_r_tl :
-  forall {A B: Type} (l: list (A * B)) (p: B) (o: A) (v: B),
-    In p (snd (split l)) ->
-    In p (snd (split ((o, v) :: l))).
-Proof.
-  induction l. crush.
-  crush.
-  specialize (IHl p o v).
-  destruct (split l). crush.
-Qed.
-
-Lemma in_split_exists_fst :
-  forall {A B: Type} (l: list (A * B)) (p: B) (eq_dec: forall n m : B, {n = m} + {n <> m}),
-    In p (snd (split l)) ->
-    exists v, In (v, p) l.
-Proof.
-  induction l; intros.
-  * intuition.
-  * destruct a.
-    destruct (eq_dec p b).
-    - exists a. crush.
-    - assert (exists v : A, In (v, p) l).
-      + apply IHl. intuition.
-        destruct (split l) eqn:?.
-        unfold snd in *.
-        simpl in H.
-        rewrite Heqp0 in H.
-        crush.
-      + destruct H0.
-        exists x.
-        crush.
-Qed.
 
 Lemma heap_get_in_split :
   forall h p l,
